c6af050da512a902ae2e9628efd1b993b11dcf6a
Shaun <seyyes2000@hotmail.com>
2020-08-23 16:16:13 -0400

    work on functions to merge two DFs

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 
 A different look at US covid data provided by Johns Hopkins.
 
-Sources:
+## Sources:
 1. https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports
 2. https://www2.census.gov/library/publications/2011/compendia/usa-counties/excel/LND01.xls
+3. For Maps: see NY times map and -- https://docs.mapbox.com/api/maps/
 
-* For land area, use column L in the excel file and convert to km if need be.
-
-
-TODO:
+## TODO:
 1. Collect latest data from github, save into csv, use text to columns by comma in csv
 2. Read both csv with pandas and create master dataframe with active cases per km calculation (new column)
-3. Review NY Times implementation of mapbox covid maps
+3. For land area, use column L in the excel file and convert to km if need be.
+4. Review NY Times implementation of mapbox covid maps
--- models/cases_sq_km.py
+++ models/cases_sq_km.py
@@ -2,5 +2,47 @@
 Calculate activate case per sq km of US counties
 """
 
+import pandas as pd
+# pd.set_option('display.precision', 10)
+
+
+
+def get_land_df(path, km_sq_to_mi):
+    df = pd.read_csv(path, usecols=['Areaname', 'LND110210D'])
+    df['km_sq'] = df.LND110210D * km_sq_to_mi
+    df.rename(columns={'LND110210D': 'mi_sq'}, inplace=True)
+    # round to two decimal places in python pandas 
+    pd.options.display.float_format = '{:.2f}'.format
+    return df
+
+
+def get_cases_df(path):
+    df = pd.read_csv(path)
+
+
+if __name__ == '__main__':
+    land_path = '../data/us_land_area/us_county_land_area.csv'
+    cases_path = '../data/us_daily/2020-08-22.csv'
+    km_sq_to_mi = 2.589975
+
+    df = get_land_df(land_path, km_sq_to_mi)
+
+    df2 = get_cases_df(cases_path)
+
+    # import pdb
+    # pdb.set_trace()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
